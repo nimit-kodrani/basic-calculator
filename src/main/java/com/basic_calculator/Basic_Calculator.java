9d5d844dfda8831118b15db05460ecf63188d163
k-r-i-n-a <116078000+k-r-i-n-a@users.noreply.github.com>
2022-10-18 18:29:11 +0530

    Implementing mode functionality
    Implementing mode functionality

--- src/main/java/com/basic_calculator/Basic_Calculator.java
+++ src/main/java/com/basic_calculator/Basic_Calculator.java
@@ -135,7 +135,18 @@ public class Basic_Calculator {
 
         buttonMode = initBtn("%", x[2], y[1], event -> {
             repaintFont();
-            // yet to be implemented
+            if (Pattern.matches("([-]?\\d+[.]\\d*)|(\\d+)", inText.getText()))
+            if (go) {
+                    val = calc(val, inText.getText(), opt);
+                    if (Pattern.matches("[-]?[\\d]+[.][0]*", String.valueOf(val))) {
+                        inText.setText(String.valueOf((int) val));
+                    } else {
+                        inText.setText(String.valueOf(val));
+                    }
+                    opt = '%';
+                    go = false;
+                    addWrite = false;
+                }
         });
 
         button4 = initBtn("4", x[0], y[3], event -> {
